fef62ba184002eb1b89a99c0c06c7281630ed655
LashanSachintha <84861849+LashanSachintha@users.noreply.github.com>
2024-05-13 10:04:35 +0530

    edit profile completed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,6 +10,13 @@ use Illuminate\Support\Facades\Validator;
 class UserController extends Controller
 {
 
+    public function profile(Request $request)
+    
+    {
+        $user = $request->session()->get('user');
+        return view('user.profile', ['user' => $user]);
+    }
+    
     public function register()
     {
         return view('user.register');
@@ -20,15 +27,20 @@ class UserController extends Controller
         return view('user.login');
     }
 
+    public function userEdit()
+    {
+        return view('user.edit');
+    }
+
     public function save(Request $request)
     {
         $rules = [
-            'first_name' => 'required|string|max:255',
-            'username' => 'required|string|max:255',
-            'contact_no' => 'required|string|max:255',
-            'address' => 'required|string|max:255',
-            'email' => 'required|string|email|unique:users|max:255',
-            'password' => 'required|string|min:6|confirmed',
+            'first_name' => 'string|max:255',
+            'username' => 'string|max:255',
+            'contact_no' => 'string|max:255',
+            'address' => 'string|max:255',
+            'email' => 'string|email|unique:users|max:255',
+           
         ];
 
         $validator = Validator::make($request->all(), $rules);
@@ -52,11 +64,45 @@ class UserController extends Controller
         return redirect()->route('home');
     }
 
+    public function update(Request $request)
+    {
+        $rules = [
+            'first_name' => 'string|max:255',
+            'username' => 'string|max:255',
+            'contact_no' => 'string|max:255',
+            'address' => 'string|max:255',
+            'email' => 'string|email|max:255',
+           
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+        
+        $oldUser = session()->get('user');
+        $user = $oldUser->editDetails(
+            
+                $request->input('first_name'),
+                $request->input('last_name'),
+                $request->input('username'),
+                $request->input('email'),
+                $request->input('address'),
+                $request->input('city'),
+                $request->input('country'),
+                $request->input('contact_no'),
+                $request->input('about')
+        );
+
+        $request->session()->put('user', $user);
+        return redirect()->route('user.profile');
+        
+    }
+
     public function loginuser(Request $request)
     {
 
         $rules = [
-
             'email' => 'required|string|email|max:255',
             'password' => 'required|string',
         ];
@@ -66,14 +112,16 @@ class UserController extends Controller
             return redirect()->back()->withErrors($validator)->withInput();
         }
 
-        $user = User::where('email', $request->email)->first();
-        if ($user) {
-            if (Hash::check($request->password, $user->password)) {
-                return redirect()->route('home');
-            }
+        //Call login function in user model
+        $user = new User();
+        $loggedInUser = $user->login($request->email, $request->password);
+
+        if ($loggedInUser) {
+            $request->session()->put('user', $loggedInUser);
+            return redirect()->route('user.profile');
         }
-        //return redirect()->route('login/{error=1}');
-        //return redirect()->back()->withErrors($validator)->withInput();
+
         return redirect()->back()->withErrors(['email' => 'Invalid credentials'])->withInput();
     }
+    
 }
