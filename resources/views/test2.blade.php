0ae17daed46dc42c3764da1aad1f8a186f7af870
malki <90499866+Malkimadhu@users.noreply.github.com>
2024-07-05 23:04:05 +0530

    new changes

--- resources/views/test2.blade.php
+++ resources/views/test2.blade.php
@@ -1,6 +1,20 @@
 {{-- -------------------------------------------------Malki -------------------------------------------------------------}}
 
-
+<nav id="menu">
+		<div class="container">
+			<div class="trigger"></div>
+			<ul>
+				<li><a href="products.html">New collection</a></li>
+				<li><a href="{{ route('shop.necklaces') }}">necklaces</a></li>
+				<li><a href="products.html">earrings</a></li>
+				<li><a href="{{ route('events.home') }}">Events</a></li>
+				<li><a href="{{ route('aboutus') }}">About</a></li>
+                <li><a href="{{ route('contactus') }}">Contact Us</a></li>
+				<li><a href="products.html">Promotions</a></li>
+			</ul>
+		</div>
+		<!-- / container -->
+	</nav>
 
 
 
