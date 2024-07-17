5a900f2104155c3a6ee67a0ace58677783a433b7
Migara Thiyunuwan <86980643+MigaraThiyunuwan@users.noreply.github.com>
2024-07-17 23:05:07 +0530

    3d model

---
+++ public/js/model.js
@@ -0,0 +1,102 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.outputColorSpace = THREE.SRGBColorSpace;
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x808080);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.set(-3, 0, 11);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.minDistance = 2;
+controls.maxDistance = 20;
+//controls.minPolarAngle = 0.5;
+//maxPolarAngle = 1.5;
+controls.autoRotate = false;
+controls.target = new THREE.Vector3(0, 1, 0);
+controls.update();
+
+const groundGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
+groundGeometry.rotateX(-Math.PI / 2);
+const groundMaterial = new THREE.MeshStandardMaterial({
+  color: 0xE0E0E0,
+  side: THREE.DoubleSide
+});
+const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
+groundMesh.castShadow = false;
+groundMesh.receiveShadow = false;
+//scene.add(groundMesh);
+
+const spotLight = new THREE.SpotLight(0xffffff, 3000, 100, 0.22, 1);
+spotLight.position.set(0, 25, 0);
+spotLight.castShadow = true;
+spotLight.shadow.bias = -0.0001;
+scene.add(spotLight);
+
+
+const directionalLight = new THREE.DirectionalLight(0xFFE955, 10);
+directionalLight.position.set(0, 0, 1); // Light direction (from front)
+scene.add(directionalLight);
+
+const directionalLight2 = new THREE.DirectionalLight(0xFFE955, 10);
+directionalLight2.position.set(0, 0, -1); // Light direction (from back)
+scene.add(directionalLight2);
+
+const directionalLight3 = new THREE.DirectionalLight(0xFFE955, 10);
+directionalLight3.position.set(1, 0, 0); // Light direction (from right)
+scene.add(directionalLight3);
+
+const directionalLight4 = new THREE.DirectionalLight(0xFFE955, 10);
+directionalLight4.position.set(-1, 0, 0); // Light direction (from left)
+scene.add(directionalLight4);
+
+const loader = new GLTFLoader().setPath('/storage/models/1/');
+loader.load('scene.gltf', (gltf) => {
+  console.log('loading model');
+  const mesh = gltf.scene;
+
+  mesh.traverse((child) => {
+    if (child.isMesh) {
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
+
+  mesh.position.set(-2, 1.05, 0);
+  scene.add(mesh);
+
+  document.getElementById('progress-container').style.display = 'none';
+}, (xhr) => {
+  console.log(`loading ${xhr.loaded / xhr.total * 100}%`);
+}, (error) => {
+  console.error(error);
+  console.log('loading eroor');
+});
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+animate();
