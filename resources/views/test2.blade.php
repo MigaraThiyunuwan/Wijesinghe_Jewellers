2711eec071692b49310b102cc1a7c39e1604234a
malki <90499866+Malkimadhu@users.noreply.github.com>
2024-10-09 11:38:42 +0530

    new update M

--- resources/views/test2.blade.php
+++ resources/views/test2.blade.php
@@ -1,25 +1,13 @@
 {{-- -------------------------------------------------Malki -------------------------------------------------------------}}
 
-
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
-
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
-|
-*/
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+jnsklnklndlkdnflkn @{{ djiuwdjinfjdn
+jdnfjdnjndscd }}]
+mcd msd vjnd sjmvn jmlsdn vjsdnvjnsdjnvc
+cmsmssckancknskncjsdn <var>
+    mksackdnjdnv
 
 
+mckdnnk</var>
 
 
 
